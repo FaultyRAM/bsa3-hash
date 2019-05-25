e76a29918c36fec9fcd6dd7f0ea53cff0ec736ca
FaultyRAM <explodingpsu@gmail.com>
2019-05-25 10:35:22 +1000

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+// Copyright (c) 2019 FaultyRAM
+//
+// Licensed under the Apache License, Version 2.0
+// <LICENSE-APACHE or http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your option. This file may not be copied,
+// modified, or distributed except according to those terms.
+
+//! The hash function used in BSA files for *The Elder Scrolls III: Morrowind*.
+
+#![no_std]
+#![forbid(
+    warnings,
+    future_incompatible,
+    rust_2018_idioms,
+    rustdoc,
+    unused,
+    missing_copy_implementations,
+    missing_debug_implementations,
+    missing_docs,
+    trivial_casts,
+    trivial_numeric_casts,
+    unsafe_code,
+    unused_results,
+    clippy::all
+)]
