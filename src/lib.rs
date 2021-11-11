5a0d75956d8a3a9f2bea729c15aafa18441cf427
FaultyRAM <316014+FaultyRAM@users.noreply.github.com>
2021-11-12 08:43:27 +1000

    fix: update to Rust 2021

--- src/lib.rs
+++ src/lib.rs
@@ -8,20 +8,15 @@
 //! The hash function used in BSA files for *The Elder Scrolls III: Morrowind*.
 
 #![no_std]
-#![forbid(
+#![deny(
     warnings,
     future_incompatible,
-    rust_2018_idioms,
-    rustdoc,
     unused,
-    missing_copy_implementations,
-    missing_debug_implementations,
-    missing_docs,
-    trivial_casts,
-    trivial_numeric_casts,
     unsafe_code,
-    unused_results,
-    clippy::all
+    clippy::all,
+    clippy::cargo,
+    clippy::pedantic,
+    rustdoc::all
 )]
 
 #[inline]
